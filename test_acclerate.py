a315276c371795a002a6d76f02c7b23f536970c3
kiendn1 <trungkienty2001@gmail.com>
2024-12-31 09:54:00 +0000

    test

---
+++ test_acclerate.py
@@ -0,0 +1,31 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from accelerate import Accelerator
+from accelerate.utils import set_seed
+from accelerate.utils import DataLoaderConfiguration
+from accelerate.utils import DistributedDataParallelKwargs
+
+# Custom Dataset
+class SequenceDataset(Dataset):
+    def __init__(self, start, end):
+        self.data = torch.arange(start, end + 1)  # Sequence from start to end
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+# Initialize the dataset and DataLoader
+dataset = SequenceDataset(1, 50)
+dataloader = DataLoader(dataset, batch_size=4, shuffle=False)
+
+# Iterate through the DataLoader
+set_seed(42)
+dataloader_config = DataLoaderConfiguration()
+dataloader_config.split_batches=True
+kwargs = DistributedDataParallelKwargs(find_unused_parameters=True)
+accelerator = Accelerator(kwargs_handlers=[kwargs], step_scheduler_with_optimizer=False, dataloader_config=dataloader_config)
+
+for batch in dataloader:
+    print(batch)
