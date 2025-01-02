66a608b1f2f766b1d3a2a812bc5139d819e8df41
kiendn1 <trungkienty2001@gmail.com>
2025-01-02 23:44:54 +0700

    hhhhhhhhh

--- main.py
+++ main.py
@@ -255,8 +255,8 @@ def train(source_loader, gendata_loader, target_train_loader, target_test_loader
             data_source, label_source, src_index = next(iter_source) # .next()
             print('src_index ', src_index)
             data_source, label_source = data_source.to(args.device), label_source.to(args.device)
-            data_source = data_source[16:]
-            label_source = label_source[16:]
+            data_source = data_source[8:]
+            label_source = label_source[8:]
             if args.gendata_dir:
                 data_gen_st, label_gen_st, gen_index = next(iter_gen)
                 data_gen_st = data_gen_st[16:]
